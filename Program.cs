97fa55a6f9fb04828400a8527ac42698d7fecc8f
unknown <brenomax02@gmail.com>
2020-06-25 10:42:06 -0300

    finalizado/Adicionando Tribonacci

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Aula25Recursividade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Recursividade recu = new Recursividade();
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("---------------Fibonacci-----------------");
+            recu.GerarSequenciaDeFibonacci(0,1,15);
+            System.Console.WriteLine("----------------Fatorial-----------------");
+            System.Console.WriteLine(recu.GerarFatorial(5));
+            Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("----------------Tribonacci----------------");
+            recu.GerarSequenciaDeTribonacci(0,2,3,5);
         }
     }
 }
